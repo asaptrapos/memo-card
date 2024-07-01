475e140c4abce5b40260a727a6953dfc6bad60fb
Egor Gorbachev <7gorbachevm@gmail.com>
2024-07-01 10:10:31 +0700

    Tests for shared code between FE / BE (#60)

--- src/lib/typescript/assert.ts
+++
@@ -1,8 +0,0 @@
-export function assert(
-  condition: any,
-  message: string = "no additional info provided",
-): asserts condition {
-  if (!condition) {
-    throw new Error("Assertion Error: " + message);
-  }
-}
