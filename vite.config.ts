4e6708a43b04196cf5237f3caaa786660682562f
Gorbachev Egor <7gorbachevm@gmail.com>
2024-08-15 16:24:38 +0700

    Try multiple entry points

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'admin/index.html'),
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
