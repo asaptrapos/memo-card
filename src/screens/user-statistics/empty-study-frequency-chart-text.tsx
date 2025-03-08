4d31c0300779daa3934887fe49e139ebad7f2ab8
Gorbachev Egor <7gorbachevm@gmail.com>
2025-03-08 11:43:05 +0700

    Fix tailwind -translate doesn't work without @base file

--- src/screens/user-statistics/empty-study-frequency-chart-text.tsx
+++ src/screens/user-statistics/empty-study-frequency-chart-text.tsx
@@ -3,7 +3,8 @@ import { t } from "../../translations/t.ts";
 export const EmptyStudyFrequencyChartText = () => {
   return (
     <div
-      className="bg-bg text-text p-3 shadow rounded-[12px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm w-[250px] text-center"
+      style={{ transform: "translate(-50%, -50%)" }}
+      className="bg-bg text-text p-3 shadow rounded-[12px] absolute top-1/2 left-1/2 text-sm w-[250px] text-center"
     >
       {t("user_stats_empty_text")}
     </div>
--- src/ui/progress-bar.tsx
+++ src/ui/progress-bar.tsx
@@ -1,12 +1,14 @@
 import { m } from "framer-motion";
 import { LazyLoadFramerMotion } from "../lib/framer-motion/lazy-load-framer-motion.tsx";
+import { observer } from "mobx-react-lite";
 
 type Props = {
   value: number;
   max: number;
 };
 
-export const ProgressBar = ({ value, max }: Props) => {
+export const ProgressBar = observer((props: Props) => {
+  const { value, max } = props;
   return (
     <div className="w-full bg-bg rounded-xl relative overflow-hidden">
       <LazyLoadFramerMotion>
@@ -16,9 +18,14 @@ export const ProgressBar = ({ value, max }: Props) => {
           className="bg-success h-[30px]"
         />
       </LazyLoadFramerMotion>
-      <div className="absolute left-1/2 top-1 -translate-x-1/2 text-center text-sm font-semibold text-text">
+      <div
+        style={{
+          transform: "translateX(-50%)",
+        }}
+        className="absolute left-1/2 top-1 text-center text-sm font-semibold text-text"
+      >
         {value}/{max}
       </div>
     </div>
   );
-};
+});
